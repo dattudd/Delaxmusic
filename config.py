987201503186b7604e4b407f6a3f4e7834b44e29
STRANGEROP <139733000+Shivasengar12@users.noreply.github.com>
2024-02-03 16:34:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ItszStranger/SHUKLA-MUSIC",
+    "https://github.com/Shivasengar12/ALEXA-NUSICS",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
