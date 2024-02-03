4d71d1fcded625332a42918df338458f4ebe514b
ItszStranger <157690917+ItszStranger@users.noreply.github.com>
2024-02-03 14:22:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,14 +35,14 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SHIVANSH474/SHUKLA-MUSIC",
+    "https://github.com/ItszStranger/SHUKLA-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SHIVANSH474")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/strangers_bots")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mastiwithfriendsx")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
