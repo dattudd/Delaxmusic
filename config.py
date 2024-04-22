fccf30cd49efcbb0882b362bdfb568d2d56a5d3d
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-04-22 14:27:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "7151436558:AAFcEXxQwnlOZFD_1SSVHhG1Ubdt-2QgOx4" )
 
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME", "Melaniarobot")
+BOT_USERNAME = getenv("BOT_USERNAME", "Hellrobot")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://VamsixD:VamsixD@vamsi.x7gyybv.mongodb.net/?retryWrites=true&w=majority")
@@ -22,10 +22,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://VamsixD:VamsixD@vamsi.x7gyy
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID", ""))
 
 # Get this value from @Melaniarobot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6919199044))
+OWNER_ID = int(getenv("OWNER_ID", ))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
